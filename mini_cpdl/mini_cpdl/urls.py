0ccacb14a967fa7ed3d6c0e5ffcfa9f32b26d962
Evan Palmer <evjpalmer@gmail.com>
2016-03-10 16:39:49 -0800

    add index url and view

--- mini_cpdl/mini_cpdl/urls.py
+++ mini_cpdl/mini_cpdl/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from scorelib import views as scorelibviews
 
 urlpatterns = [
+    url(r'^$', scorelibviews.index),
     url(r'^admin/', admin.site.urls),
 ]
