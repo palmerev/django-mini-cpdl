ddaaff494558e6cc19369d24494b7e3e29fb601f
Evan Palmer <evjpalmer@gmail.com>
2016-03-12 17:05:12 -0800

    add form elements and fake search results to index.html

--- mini_cpdl/tests_func.py
+++ mini_cpdl/tests_func.py
@@ -18,19 +18,39 @@ class MusicianTestCase(LiveServerTestCase):
         home_page = self.browser.get(self.live_server_url + '/')
         # She knows she's in the right place because she can see the name of the
         # site in the browser chrome.
-        brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
+        brand_element = self.browser.find_element_by_css_selector(
+            '.navbar-brand')
         self.assertEqual('miniScoreLib', brand_element.text)
-        self.fail('Incomplete Test')
         # She sees the inputs of the search form, including labels
         # and placeholders.
+        composer_input = self.browser.find_element_by_css_selector(
+            'input#composer-field')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="composer-field"]'))
+        self.assertEqual(composer_input.get_attribute('placeholder'),
+            'i.e. Byrd')
+        voicing_input = self.browser.find_element_by_css_selector(
+            'input#voicing-field')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="voicing-field"]'))
+        self.assertEqual(voicing_input.get_attribute('placeholder'), 'i.e. SATB')
 
         # She types in the name of a composer, and submits it.
-
+        composer_input.send_keys('Lasso')
+        self.browser.find_element_by_css_selector('form button').click()
         # She sees too many search results...
-
+        search_results = self.browser.find_elements_by_css_selector(
+            '.search-result')
+        self.assertGreater(len(search_results), 2)
         # ...so she adds a voicing to her search query and gets a more
         # manageable list.
-
+        voicing_input = self.browser.find_element_by_css_selector(
+            'input#voicing-field')
+        voicing_input.send_keys('SATB')
+        search_results2 = self.browser.find_elements_by_css_selector(
+            '.search-result')
+        self.assertEqual(len(search_results2), 2)
+        self.fail('Incomplete Test')
         # She clicks on a search result.
 
         # The piece's page has the title, composer, and voicing of
