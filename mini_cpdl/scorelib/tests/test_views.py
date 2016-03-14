139d771b2a8af04a1ef3efdfbadd70b15c520bee
Evan Palmer <evjpalmer@gmail.com>
2016-03-13 17:07:39 -0700

    add test_index_view_returns_scores, fake QuerySet for Score (not implemented yet)

--- mini_cpdl/scorelib/tests/test_views.py
+++ mini_cpdl/scorelib/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, RequestFactory
+from django.db.models.query import QuerySet
 
 from scorelib.views import index
 
@@ -14,3 +15,17 @@ class IndexViewTestCase(TestCase):
         with self.assertTemplateUsed('scorelib/index.html'):
             response = index(request)
             self.assertEqual(response.status_code, 200)
+
+    def test_index_view_returns_scores(self):
+        """Test that the index view attempts to return Scores if query
+        parameters exist"""
+        # use the test client instead of factory so we can
+        # access the response.context dictionary
+        response = self.client.get(
+            '/',
+            {'composer': 'Dowland'}
+        )
+        self.assertIs(
+            type(response.context['scores']),
+            QuerySet
+        )
--- mini_cpdl/scorelib/views.py
+++ mini_cpdl/scorelib/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('scorelib/index.html')
+    context = {'scores': Scores.objects.all()}
+    return render_to_response('scorelib/index.html', context)
