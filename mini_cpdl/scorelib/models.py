3f65b43b9eafcbc4127fc46f7c09cd97baf94a7b
Evan Palmer <evjpalmer@gmail.com>
2016-03-15 13:10:51 -0700

    add Score model and migrations

--- mini_cpdl/scorelib/models.py
+++ mini_cpdl/scorelib/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Score(models.Model):
+    title = models.CharField(max_length=100)
+    composer = models.CharField(max_length=100)
+    voicing = models.CharField(max_length=100)
