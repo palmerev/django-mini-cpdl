1ac8de49df7dcdfb388bcebcb91246fd2559f96b
Evan Palmer <evjpalmer@gmail.com>
2016-03-15 15:34:10 -0700

    add filter functionality, debug-level logging to index view

--- mini_cpdl/scorelib/views.py
+++ mini_cpdl/scorelib/views.py
@@ -1,11 +1,36 @@
+import logging
+
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 from .models import Score
 
 
+logging.basicConfig(
+    filename='views.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(levelname)s:%(message)s'
+)
+
+
 def index(request):
-    context = {'scores': Score.objects.filter(
-        voicing=request.GET.get('voicing', None)
-    )}
+    context = {'scores': []}
+    if request.GET:
+        logging.debug("request.GET: %s", request.GET)
+        scores_queryset = Score.objects.all()
+        if request.GET.get('voicing', None):
+            logging.debug("voicing filter ran with voicing: %s", request.GET['voicing'])
+            scores_queryset = scores_queryset.filter(
+                voicing=request.GET.get('voicing', None)
+            )
+        if request.GET.get('composer', None):
+            composer = request.GET['composer']
+            logging.debug("composer filter ran with composer: %s", composer)
+            scores_queryset = scores_queryset.filter(
+                composer__icontains=composer
+            )
+
+        context['scores'] = scores_queryset
+
     return render_to_response('scorelib/index.html', context)
