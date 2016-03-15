000f37422dbdbb95c743790e66d8031f5630308a
Evan Palmer <evjpalmer@gmail.com>
2016-03-15 15:30:32 -0700

    add __str__ method to Score model

--- mini_cpdl/scorelib/models.py
+++ mini_cpdl/scorelib/models.py
@@ -5,3 +5,6 @@ class Score(models.Model):
     title = models.CharField(max_length=100)
     composer = models.CharField(max_length=100)
     voicing = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.composer + ": " + self.title + " (" + self.voicing + ")"
